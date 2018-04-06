8bcf249388543e2cfbf8e234a19124b1dad7c660
Mike Schatz <mschatz@cs.jhu.edu>
2018-04-06 01:05:12 -0400

    fix when no queries are listed

--- Nchart.R
+++ Nchart.R
@@ -12,33 +12,6 @@ if (length(args) < 2) {
 # output prefix
 outfile <- args[[1]]
 
-query_filenames = tail(args, -1)
-query_names <- query_filenames
-
-## Assume the first entry is the reference
-filename_ref <- query_filenames[[1]]
-
-cat("Creating Nchart\n")
-cat("===============\n")
-cat("outfile: ",         outfile, "\n")
-cat("filename_ref: ",    filename_ref, "\n")
-cat("query_filenames: ", query_filenames, "\n")
-cat("query_names: ",     query_names, "\n")
-
-
-stop()
-
-library(ggplot2)
-library(scales)
-
-
-# first color is the reference, the others are the queries in order
-colors <- rainbow(length(query_filenames))
-=======
-### output prefix
-outfile <- args[[1]]
-
-## refid 
 ref_filename <- args[[2]]
 ref_name <- ref_filename
 
@@ -53,9 +26,6 @@ cat("Creating Nchart\n")
 cat("===============\n")
 cat("outfile: ",         outfile, "\n")
 cat("ref_filename: ",    ref_filename, "\n")
-cat("query_filenames: ", query_filenames, "\n")
-cat("query_names: ",     query_names, "\n")
-
 
 
 ## Load the reference lens
@@ -70,23 +40,21 @@ ref.cumsum.0 <- rbind(data.frame(NG=c(0),contig.length=max(ref.cumsum$contig.len
 ## Now process each query
 ############################################################################
 
-for (i in seq(length(query_filenames))) {
-  filename_query = query_filenames[i]
-  name_query = query_names[i]
-  print(paste(filename_query,name_query))
-  
-  query.data <- read.table(filename_query, header=FALSE)
-  
-  names(query.data) <- c("name","length")
-  
-  query.data$length <- sort(as.numeric(query.data$length),decreasing=TRUE)
-  
-  query.cumsum <- data.frame(NG=cumsum(query.data$length/genome.length*100),contig.length=query.data$length,contig.source=name_query)
-  
-  query.cumsum.0 <- rbind(data.frame(NG=c(0),contig.length=max(query.cumsum$contig.length),contig.source=name_query),query.cumsum)
-  
-  ref.cumsum <- rbind(ref.cumsum,query.cumsum)
-  ref.cumsum.0 <- rbind(ref.cumsum.0,query.cumsum.0)
+if (length(query_filenames) > 0)
+{
+  for (i in seq(length(query_filenames))) {
+    filename_query = query_filenames[i]
+    name_query = query_names[i]
+    cat("query: ", i, " ", filename_query, " ", name_query, "\n")
+    
+    query.data <- read.table(filename_query, header=FALSE)
+    names(query.data) <- c("name","length")
+    query.data$length <- sort(as.numeric(query.data$length),decreasing=TRUE)
+    query.cumsum <- data.frame(NG=cumsum(query.data$length/genome.length*100),contig.length=query.data$length,contig.source=name_query)
+    query.cumsum.0 <- rbind(data.frame(NG=c(0),contig.length=max(query.cumsum$contig.length),contig.source=name_query),query.cumsum)
+    ref.cumsum <- rbind(ref.cumsum,query.cumsum)
+    ref.cumsum.0 <- rbind(ref.cumsum.0,query.cumsum.0)
+  }
 }
 
 
@@ -108,13 +76,13 @@ percent_format <- function(num) {
   paste(num,"%", sep="")
 }
 
-cat("make plot\n")
+cat("make plot: ", outfile, "\n")
 
 png(file=outfile, width=1600,height=1000,res=200)
 
 print(
   ggplot(ref.cumsum.0, aes(x = NG, y = contig.length, color=contig.source)) + 
-    #             xlim(0,100) +
+     #            xlim(0,100) +
     scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x), labels = trans_format("log10", math_format(10^.x)), limits=c(1,genome.length)) + 
     scale_x_continuous(labels=percent_format) +
     geom_path(size=1.5,alpha=0.5) + 
